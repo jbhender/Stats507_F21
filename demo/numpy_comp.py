f4f0d782debd05513e7699e94a5282f398831210
jbhender <jbhender@umich.edu>
2021-09-14 10:50:52 -0400

    Refactor of numpy_comp demo.

--- demo/numpy_comp.py
+++ demo/numpy_comp.py
@@ -50,7 +50,11 @@ def mult_zip(a, b):
     The element-wise product, `a * b`, with class the same as `a`.
 
     """
-    return(a.__class__([i * j for i, j in zip(a, b)]))
+    out = [i * j for i, j in zip(a, b)]
+    if isinstance(a, np.ndarray):
+        return(out)
+    else:
+        return(a.__class__(out))
 
 
 def mult_enum(a, b):
@@ -67,7 +71,12 @@ def mult_enum(a, b):
     The element-wise product, `a * b`, with class the same as `a`.
 
     """
-    return(a.__class__([j * b[i] for i, j in enumerate(a)]))
+    out = [j * b[i] for i, j in enumerate(a)]
+    if isinstance(a, np.ndarray):
+        return(out)
+    else:
+        return(a.__class__(out))
+    return(a.__class__())
 
 
 # a numpy implementation: ----------------------------------------------------
@@ -85,48 +94,44 @@ def mult_np(a, b):
     The element-wise product, `a * b`, with class the same as `a`.
 
     """
-    return(a.__class__(np.asarray(a) * np.asarray(b)))
+    out = np.asarray(a) * np.asarray(b)
+    if isinstance(a, np.ndarray):
+        return(out)
+    else:
+        return(a.__class__(out))
 
 
 # confirm functions return the same values: ----------------------------------
+# test with ndarray input
 n = 10000
-a = list(np.linspace(0, 1, num=n))
-b = list(np.linspace(0, 2 * np.pi, num=n))
+a = np.linspace(0, 1, num=n)
+b = np.linspace(0, 2 * np.pi, num=n)
 
 c0 = mult_doc(a, b)
 c1 = mult_zip(a, b)
 c2 = mult_enum(a, b)
 c3 = mult_np(a, b)
 
-assert c0 == c1 == c2 == list(c3)
+assert np.all(c0 == c1 == c2 == c3)
 
-# timing: --------------------------------------------------------------------
+# test with list input
+a = list(a)
+b = list(b)
 
-# list input
-time = defaultdict(list)
-for f in [mult_doc, mult_zip, mult_enum, mult_np]:
-    t = Timer('f(a, b)', globals={'f': f, 'a': a, 'b': b})
-    tm = t.repeat(repeat=10, number=1000)
-    time['Function'].append(f.__name__)
-    time['min, s'].append(np.min(tm))
-    time['median, s'].append(np.median(tm))
-    time['mean, s'].append(np.mean(tm))
+c0 = mult_doc(a, b)
+c1 = mult_zip(a, b)
+c2 = mult_enum(a, b)
+c3 = mult_np(a, b)
 
-time = pd.DataFrame(time)
+assert c0 == c1 == c2 == c3
 
-for c, d in zip(time.columns, time.dtypes):
-    if d == np.dtype('float64'):
-        time[c] = time[c].map(lambda x: '%5.3f' % x)
-
-print(time.to_markdown(index=False))
-
-# ndarray input
+# timing with ndarray input: -------------------------------------------------
 a = np.asarray(a)
 b = np.asarray(b)
 time_nda = defaultdict(list)
 for f in [mult_doc, mult_zip, mult_enum, mult_np]:
     t = Timer('f(a, b)', globals={'f': f, 'a': a, 'b': b})
-    tm = t.repeat(repeat=10, number=1000)
+    tm = t.repeat(repeat=10, number=100)
     time_nda['Function'].append(f.__name__)
     time_nda['min, s'].append(np.min(tm))
     time_nda['median, s'].append(np.median(tm))
@@ -136,5 +141,22 @@ time_nda = pd.DataFrame(time_nda)
 for c, d in zip(time_nda.columns, time_nda.dtypes):
     if d == np.dtype('float64'):
         time_nda[c] = time_nda[c].map(lambda x: '%5.3f' % x)
+time_nda
 
-print(time_nda.to_markdown(index=False))
+# timing with list input: ----------------------------------------------------
+a = list(a)
+b = list(b)
+time_lst = defaultdict(list)
+for f in [mult_doc, mult_zip, mult_enum, mult_np]:
+    t = Timer('f(a, b)', globals={'f': f, 'a': a, 'b': b})
+    tm = t.repeat(repeat=10, number=100)
+    time_lst['Function'].append(f.__name__)
+    time_lst['min, s'].append(np.min(tm))
+    time_lst['median, s'].append(np.median(tm))
+    time_lst['mean, s'].append(np.mean(tm))
+
+time_lst = pd.DataFrame(time_lst)
+for c, d in zip(time_lst.columns, time_lst.dtypes):
+    if d == np.dtype('float64'):
+        time_lst[c] = time_lst[c].map(lambda x: '%5.3f' % x)
+time_lst
